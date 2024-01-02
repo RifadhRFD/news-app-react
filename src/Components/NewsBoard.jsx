cb51e150fb2221560029621a1b0790e49d72ef6e
Rifadh <rifadhrfd@gmail.com>
2024-01-02 22:27:42 +0530

    fetching article map error fixed

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,29 +1,53 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import NewsItem from "./NewsItem";
+import NewsItem from './NewsItem';
 
-const NewsBoard = ({category}) => {
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
 
-    const [articles, setArticles] = useState([]);
-
-    useEffect(()=> {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles))
-    },[category])
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data.articles)) {
+          setArticles(data.articles);
+        } else {
+          // Handle the case when 'articles' is not an array
+          console.error('Invalid articles data:', data.articles);
+        }
+      })
+      .catch((error) => {
+        // Handle fetch errors
+        console.error('Error fetching data:', error);
+      });
+  }, [category]);
 
   return (
     <div>
-      <h2 className="text-center fs-3">Latest<span className="badge text-danger fs-3">News</span></h2>
-      {articles.map((news,index)=>{
-        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+      <h2 className="text-center fs-3">
+        Latest<span className="badge text-danger fs-3">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 NewsBoard.propTypes = {
-    category: PropTypes.string.isRequired,  // Add prop validation for 'category'
-  };
+  category: PropTypes.string.isRequired, // prop validation for 'category'
+};
 
-export default NewsBoard
+export default NewsBoard;
